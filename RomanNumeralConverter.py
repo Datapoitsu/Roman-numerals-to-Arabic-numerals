98c440073b7b98f2e49bc7aa5e87e8abb964e1b7
Aarni Junkkala <83189693+Datapoitsu@users.noreply.github.com>
2026-05-09 17:22:10 +0300

    Major rewrite

--- RomanNumeralConverter.py
+++ RomanNumeralConverter.py
@@ -1,190 +1,171 @@
-# -------------------- Roman to Arabic to Roman number converter -------------------- ##
+# -------------------- Roman Arabic Converter -------------------- ##
 #Written by: Aarni Junkkala
 #Code turns numbers from Roman to Arabic and vice versa.
-#Limit of Roman numbers is 3999 -> MMMCMXCIX, can't convert numbers higer than that.
+#Limit of Roman numbers is from 1 to 3999 -> I -> MMMCMXCIX, can't convert numbers higer than that.
 
 #If you want to call this script from outside, call: Convert(str || int)
+RomanSymbols = ["I","V","X","L","C","D","M"]
 
-class Number: #Creates data of each Arabic and Roman number pair.
-    def __init__(self, Roman, Arabic):  
-        self.Roman = Roman
-        self.Arabic = Arabic
-        
-Numbers = []
+def FilterValidSymbols(text:str,validSymbols:list[str]) -> str: #Keeps only symbols that are allowed
+    result = ""
+    for i in range(len(text)):
+        if text[i] in validSymbols:
+            result += text[i]
+    return result
 
-def SetuUpNumbers():
-    #Creates a list of class with all number and symbols
-    Numbers.append(Number("I",1))
-    Numbers.append(Number("V",5))
-    Numbers.append(Number("X",10))
-    Numbers.append(Number("L",50))
-    Numbers.append(Number("C",100))
-    Numbers.append(Number("D",500))
-    Numbers.append(Number("M",1000))
-
-def RomanToArabic(s):
-    global Numbers
-    s = str(s)
-    s = s.upper() #All to upper so all symbols are same
-    #Error check, can't have 4 same symbols in a row
-    for i in range(len(s)):
-        if i + 3 < len(s):
-            if s[i] == s[i + 1] and s[i] == s[i + 2] and s[i] == s[i + 3]:
-                return False
-        else:
-            break
-
-    #Makes a list of all symbols turned into arabian numbers, Example: XIV -> [10,1,5]
-    NumberList = []
-    for i in range(len(s)):
-        for k in range(len(Numbers)):
-            if s[i] == Numbers[k].Roman:
-                NumberList.append(Numbers[k].Arabic)
-                continue
-        if len(NumberList) != i + 1:
+def AllSame(arr:list) -> bool: #Are all elements same in the array
+    for i in range(1,len(arr)):
+        if arr[i] != arr[0]:
             return False
-    
-    #Error check, Numbers are in wrong order.
-    #Right: CL -> [100, 50] #Wrong: LC -> [50, 100]
-    #Right: XC -> [10, 100] #Right: CX -> [100, 10]
-    for i in range(len(NumberList)):
-        if i + 1 < len(NumberList):
-            if NumberList[i] * 10 < NumberList[i + 1]:
-                return False
-    
-    #Turns numbers into groups that are calculated at the end
-    #Example : XCIX -> [[10, 100], [1, 10]]
-    #Example2 : CLXV -> [[100], [50], [10], [5]]
-    NumberGroups = []
-    num = 0
-    while num < len(NumberList):
-        if num + 1 < len(NumberList):
-            if NumberList[num] < NumberList[num + 1]: #Smaller -> IX
-                NumberGroups.append([NumberList[num], NumberList[num + 1]])
-                num += 2
-                continue
-            if NumberList[num] >= NumberList[num + 1]: #Greater or equal -> XI || -> XX
-                NumberGroups.append([NumberList[num]])
-                num += 1
-                continue
-            if num + 2 < len(NumberList): #if three of same symbol in a row -> VIII
-                if NumberList[num] == NumberList[num + 1] and NumberList[num] == NumberList[num + 2]: #Samat 3kpl -> XXX
-                    NumberGroups.append([NumberList[num],NumberList[num + 1], NumberList[num + 2]])
-                    num += 3
-                    continue
-                elif NumberList[num] == NumberList[num + 1]: #if two of same symbol in a row -> VII
-                    NumberGroups.append([NumberList[num],NumberList[num + 1]])
-                    num += 2
-                    continue
-                    
-        else:
-            NumberGroups.append([NumberList[num]])
-            num += 1
-            continue
+    return True
 
-    #Error check, can't have a group before being lesser than a group after: WRONG: XXC -> [10, 10, 100], RIGHT: CXX ->  [100, 10, 10]
-    for i in range(len(NumberGroups)):
-        if i + 1 < len(NumberGroups):
-            if NumberGroups[i] < NumberGroups[i + 1]:
-                return False
-            
-    #Error check, can't have groub before having a subtraction and then have a next groub being equal of subtraction
-    #Example: [[10,100], [10], [1]]. Tens are wrong
-    for i in range(len(NumberGroups)):
-        if i + 1 < len(NumberGroups):
-            if len(NumberGroups[i]) > 1:
-                if NumberGroups[i][0] == NumberGroups[i + 1][0]:
-                    return False
+def isRepetitive(arr:list,count:int) -> bool:
+    for i in range(len(arr) - 3):
+        if AllSame(arr[i:i+count]):
+            return True
+    return False
 
-    #Calculates numbersets. Example: [[10,100], [1,10]] -> [90, 9]
-    NumberSets = []
-    for i in range(len(NumberGroups)):
-        if len(NumberGroups[i]) == 1: #Single number, stays the same
-            NumberSets.append(NumberGroups[i][0])
-            continue
-        if len(NumberGroups[i]) == 2: #Two numbers
-            if NumberGroups[i][0] < NumberGroups[i][1]: #if the first number is smaller, then subtrack, first from last
-                NumberSets.append(NumberGroups[i][1] - NumberGroups[i][0])
-                continue
-            if NumberGroups[i][0] >= NumberGroups[i][1]:#if same or higher, just sum up.
-                NumberSets.append(NumberGroups[i][0] + NumberGroups[i][1])
-                continue
-        if len(NumberGroups[i]) == 3: #Three numbers are summed up
-            num = 0
-            for k in range(len(NumberGroups[i])):
-                num += NumberGroups[i][k]
-            NumberSets.append(num)
-            continue
+def romanIndex(char:chr) -> int:
+    return RomanSymbols.index(char)
 
-    Result = 0
-    for i in range(len(NumberSets)):
-        Result += NumberSets[i]
-    return Result
+def romanIndexes(text:str) -> list[int]:
+    result = []
+    for i in range(len(text)):
+        result.append(romanIndex(text[i]))
+    return result
 
-def ArabicToRoman(s):
-    s = str(s)
-    #Turns arabic symbols into a list
-    #Example: 123 -> [1,2,3]
-    NumberList = []
-    for i in range(len(s)):
-        NumberList.append(int(s[len(s) - (i + 1)]))
-    NumberList = list(reversed(NumberList))
-    
-    Result = ""
-    #Looks for each conversion of each symbol on numberlist
-    for i in range(len(NumberList)):
-        Symbol = ""
-        if NumberList[i] == 0: #Zero isn't written in roman numbers
-            continue
-        if NumberList[i] == 9:
-            #Adds lowering symbols, example: 9 -> IX, 90 XC, 900 CM
-            Symbol = Numbers[(((len(NumberList) - i) - 1) * 2)].Roman
-            Result += Symbol
-            Symbol = Numbers[(((len(NumberList) - i) - 1) * 2) + 2].Roman
-            Result += Symbol
-        else: #Rest of the numbers
-            ResultHolder = ""
-            if NumberList[i] >= 4: #All numbers 4 and above gets first the half symbol (5V,50L,500D)
-                Symbol = Numbers[(((len(NumberList) - i) - 1) * 2) + 1].Roman
-                ResultHolder += Symbol
-                NumberList[i] -= 5 #Reduces the 5 from the number, so rest is worked sameway
-                
-            if NumberList[i] == -1: #If negative, then needs to have reducing symbol before main symbol
-                Symbol = Numbers[(((len(NumberList) - i) - 1) * 2)].Roman
-                ResultHolder = Symbol + ResultHolder
-                
-            if NumberList[i] >= 1: #Adds regular symbols foreach left
-                #example: 3 -> III
-                #example: 8 -> V (8-5 = 3) -> VIII
-                #example: 4 -> V (4-5 = -1) -> IV
-                Symbol = Numbers[(((len(NumberList) - i) - 1) * 2)].Roman
-                for i in range(NumberList[i]):
-                    ResultHolder += Symbol
-            Result += ResultHolder
-    return Result
+def inRange(arr:list,index:int|list[int]):
+    if type(index) == int:
+        index = [index]
+    for i in range(len(index)):
+        if index[i] < 0 or index[i] > len(arr) - 1:
+            return False
+    return True
 
-def Convert(s):
-    s = str(s) #Converts to string, if user inputs argument as an int
-    if s == "" or s == None:
+def validSymbols(text:str) -> bool:
+    for i in range(len(text)):
+        if text[i].isdigit():
+            return False
+        if text[i].isalpha() and text[i] not in RomanSymbols:
+            return False
+    return True
+
+def isLegalRoman(text:str) -> bool:
+    if isRepetitive(text,4): #Error check: Max 3 of same symbol
         return False
+    indexList = romanIndexes(text) #Converts to numbers matching indexes of roman numbers in ascending order.
+    for i in range(len(indexList)):
+        if indexList[i] % 2 == 0: #I, X, C, M = 1, 10, 100, 100
+            if inRange(indexList,i + 1) and indexList[i+1] > indexList[i] + 2: #multiple of 10 must be followed by two greater or smaller. Correct: XL, incorrect XD.
+                return False
+            if inRange(indexList, [i - 1, i + 1]) and indexList[i - 1] < indexList[i] and indexList[i + 1] == indexList[i - 1]: #After reduction can't be followed by same number as before.
+                return False
+        if indexList[i] % 2 == 1: #V, L, D = 5, 50 ,500
+            if inRange(indexList, i + 1) and indexList[i+1] > indexList[i]: #Next can't be greater.
+                return False
+            if inRange(indexList, i - 2) and indexList[i - 2] < indexList[i]: #If previos is reduced, this can't be in same range.
+                return False
+            if inRange(indexList, i + 1) and indexList[i] == indexList[i + 1]: #No repeats.
+                return False
+    return True
 
+def IndevidualRomanToArabic(roman:str): #Converts indeviual symbols to arabic numbers, Example: XIV -> [10,1,5]
+    numberList = []
+    indexList = romanIndexes(roman) #Converts to numbers matching indexes of roman numbers in ascending order.
+    for i in range(len(roman)):
+        #Adds numbers based on following table [1,5,10,50,100,500,1000]
+        numberList.append(10 ** ((indexList[i] + (indexList[i] % 2)) / 2) / (1 if indexList[i] % 2 == 0 else 2)) #What a nightmare of a formula!
+    return numberList
+
+def RomanToArabic(roman:str) -> int:
+    roman = roman.upper()
+
+    if not validSymbols(roman): #If contains invalid symbols.
+        return None
+
+    roman = FilterValidSymbols(roman, RomanSymbols) #Filtering spaces and special characters.
+        
+    if not isLegalRoman(roman):
+        return None
+    
+    numberList = IndevidualRomanToArabic(roman) #Converting all roman symbols to indevidual numbers.
+
+    #Calculates the reduction. XLIV -> [-10 + 100, -1 + 5] -> [90,4]
+    result = 0
+    iteration = iter(range(len(numberList)))
+    for i in iteration:
+        if i < len(numberList) -1 and numberList[i] < numberList[i + 1]:
+            result += numberList[i + 1] - numberList[i]
+            next(iteration)
+        else:
+            result += numberList[i]
+
+    return int(result)
+
+def ArabicToRoman(arabic:int|str) -> str:
     try:
-        s = int(s) #If it can be a integer, then it is arabicnumber
+        int(arabic)
     except:
-        s = str(s) #If it can't be an integer, then it must be romannumeral
+        return None
+    
+    numberList = list(map(int,list(str(arabic)))) #Example: 123 -> [1,2,3]
+    Result = ""
+    for i in range(len(numberList)):
+        match numberList[i]:
+            case 0:
+                continue
+            case 1 | 2 | 3:
+                for k in range(numberList[i]):
+                    Result += RomanSymbols[2 * (len(numberList) - i) - 2] #1,10,100,1000
+            case 5:
+                Result += RomanSymbols[2 * (len(numberList) - i) - 1] #5,50,500
+            case 6 | 7 | 8:
+                Result += RomanSymbols[2 * (len(numberList) - i) - 1] #5,50,500
+                for k in range(numberList[i] - 5):
+                    Result += RomanSymbols[2 * (len(numberList) - i) - 2] #1,10,100,1000
+            case 4:
+                Result += RomanSymbols[2 * (len(numberList) - i) - 2] #1,10,100,1000
+                Result += RomanSymbols[2 * (len(numberList) - i) - 1] #5,50,500
+            case 9:
+                Result += RomanSymbols[2 * (len(numberList) - i) - 2] #1,10,100,1000
+                Result += RomanSymbols[2 * (len(numberList) - i)] #1,10,100,1000
+    return Result
+
+def Convert(number:str|int) -> int|str|None:
+    if number in ["",None]:
+        return None
+    
+    try: #checking the type. Numbers can also be inputted as a string.
+        number = int(number) #If it can be a integer, then it is arabic
+    except:
+        number = str(number) #If it can't be an integer, then it could be roman
     
     #Arabic to Roman
-    if isinstance(s, int) and s > 0 and s < 4000: #Can't be too high or low
-        return ArabicToRoman(s)
+    if isinstance(number, int) and number > 0 and number < 4000: #Can't be too high or low
+        return ArabicToRoman(number)
     #Roman to Arabic
-    if isinstance(s, str):
-        s = s.replace(" ", "")
-        return RomanToArabic(s)
+    if isinstance(number, str):
+        return RomanToArabic(number)
+    return None
 
-SetuUpNumbers()
+def TestMatching(): #Tests that the functions return mirrored answers.
+    incorrect = []
+    for i in range(1,4000):
+        if RomanToArabic(ArabicToRoman(i)) != i:
+            incorrect.append(i)
+    if len(incorrect) == 0:
+        print("No errors!")
+        return
+
+    print("Incorrect at")
+    for i in range(len(incorrect)):
+        print(str(i) + " ",end="")
+
 if __name__ == '__main__':
-    while True: #Repeats for infinite
+    while True:
         print("--------------------")
         print("Roman to Arabic to Roman number converter")
         UserInput = input("Insert number: ")
+        if UserInput in ["","exit"]:
+            break
         print(Convert(UserInput))
